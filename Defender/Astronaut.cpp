daf41ff4f4624ee54e6edb36e9b13c4e576b250a
brettcarpio <ckcarpio@gmail.com>
2016-12-24 00:12:52 +0000

    Added Game Class

--- Defender/Astronaut.cpp
+++ Defender/Astronaut.cpp
@@ -1,3 +1,4 @@
+#include "stdafx.h"
 #include "Astronaut.h"
 
 Astronaut::Astronaut() : GameEntity()
---
+++ Defender/Game.cpp
@@ -0,0 +1,49 @@
+#include "stdafx.h"
+#include "Game.h"
+
+Game::Game() : m_running(false),
+	m_window(sf::VideoMode(800, 600, 32), "Defender") {
+
+}
+
+Game::~Game() {
+	delete m_sceneMgr;
+	m_window.close();
+}
+
+void Game::Initialise() {
+	m_window.setFramerateLimit(60);
+	m_sceneMgr = new SceneManager(m_window.getSize().x, m_window.getSize().y);
+	m_running = true;
+}
+
+void Game::Update() {
+	m_time = m_clock.restart();
+	m_sceneMgr->update(m_time.asMilliseconds());
+}
+
+void Game::Draw() {
+	m_window.clear();
+
+	m_sceneMgr->draw(m_window);
+
+	m_window.display();
+}
+
+void Game::CheckInput() {
+	sf::Event Event;
+	while (m_window.pollEvent(Event)){
+		if (Event.type == sf::Event::Closed	||
+			(Event.type == sf::Event::KeyPressed && Event.key.code == sf::Keyboard::Escape))
+			m_running = false;
+
+		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right) && m_sceneMgr->getCurrentScene()->getTitle() == "Menu")
+			m_sceneMgr->nextScene();
+		else if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left) && m_sceneMgr->getCurrentScene()->getTitle() == "GameOver")
+			m_sceneMgr->previousScene();
+	}
+}
+
+bool Game::IsRunning() {
+	return m_running;
+}
